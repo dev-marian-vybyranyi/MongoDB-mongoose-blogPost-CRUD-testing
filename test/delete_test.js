57b4f50c7b7aefdc852299826e9c266415bf280d
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-06 16:50:47 +0200

    Introduce `postCount` field to `User` schema and add a test for updating it.

--- test/delete_test.js
+++ test/delete_test.js
@@ -1,45 +1,45 @@
-const assert = require('assert');
-const User = require('../src/user');
+const assert = require("assert");
+const User = require("../src/user");
 
-describe('Deleting a user', () => {
+describe("Deleting a user", () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: "Joe" });
     joe.save().then(() => done());
   });
 
-  it('model instance remove', (done) => {
+  it("model instance remove", (done) => {
     joe
       .deleteOne()
-      .then(() => User.findOne({ name: 'Joe' }))
+      .then(() => User.findOne({ name: "Joe" }))
       .then((user) => {
         assert(user === null);
         done();
       });
   });
 
-  it('class method remove', (done) => {
-    User.deleteMany({ name: 'Joe' })
-      .then(() => User.findOne({ name: 'Joe' }))
+  it("class method remove", (done) => {
+    User.deleteMany({ name: "Joe" })
+      .then(() => User.findOne({ name: "Joe" }))
       .then((user) => {
         assert(user === null);
         done();
       });
   });
 
-  it('class method findOneAndRemove', (done) => {
-    User.findOneAndRemove({ name: 'Joe' })
-      .then(() => User.findOne({ name: 'Joe' }))
+  it("class method findOneAndRemove", (done) => {
+    User.findOneAndRemove({ name: "Joe" })
+      .then(() => User.findOne({ name: "Joe" }))
       .then((user) => {
         assert(user === null);
         done();
       });
   });
 
-  it('class method findByIdAndRemove', (done) => {
+  it("class method findByIdAndRemove", (done) => {
     User.findByIdAndRemove(joe._id)
-      .then(() => User.findOne({ name: 'Joe' }))
+      .then(() => User.findOne({ name: "Joe" }))
       .then((user) => {
         assert(user === null);
         done();
