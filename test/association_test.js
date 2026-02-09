8c71599e9eb5e63c39da50d3c71de0ac44526997
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-09 08:59:31 +0200

    Add test to verify user-blogpost association and persist related models in beforeEach.

--- test/association_test.js
+++ test/association_test.js
@@ -17,5 +17,18 @@ describe("Assocations", () => {
     joe.blogPosts.push(blogPost);
     blogPost.comments.push(comment);
     comment.user = joe;
+
+    Promise.all([joe.save(), blogPost.save(), comment.save()]).then(() =>
+      done(),
+    );
+  });
+
+  it.only("saves a relation between a user and a blogpost", (done) => {
+    User.findOne({ name: "Joe" })
+      .then()
+      .then((user) => {
+        console.log(user);
+        done();
+      });
   });
 });
