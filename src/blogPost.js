d55035ea3bb4805d3bcdf717edc12798189cd102
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-02-09 08:43:04 +0200

    add Comment model and comments array to BlogPost schema

--- src/blogPost.js
+++ src/blogPost.js
@@ -4,4 +4,14 @@ const Schema = mongoose.Schema;
 const BlogPostSchema = new Schema({
   title: String,
   content: String,
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "comment",
+    },
+  ],
 });
+
+const BlogPost = mongoose.model("blogPost", BlogPostSchema);
+
+module.exports = BlogPost;
